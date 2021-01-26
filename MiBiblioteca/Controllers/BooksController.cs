6b86f2213cc78a1100d3382534688aa36828c78b
Cristian Plasencia <plc.cristian@gmail.com>
2021-01-26 14:26:33 +0000

    añadidoview model

--- MiBiblioteca/Controllers/BooksController.cs
+++ MiBiblioteca/Controllers/BooksController.cs
@@ -4,24 +4,37 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MiBiblioteca.Models;
+using MiBiblioteca.ViewModels;
 
 namespace MiBiblioteca.Controllers
 {
     public class BooksController : Controller
     {
         // GET: Movies/Random
-        public ViewResult Random()
+        public ActionResult Random()
         {
             var book = new Book() { Name = "Book test"};
-            
-            return View(book);
+
+            var customers = new List<Customer>
+           {
+               new Customer { Name = "Customer 1" },
+               new Customer { Name = "Customer 2"}
+           };
+
+            var viewModel = new RandomBookViewModel
+            {
+                Book = book,
+                Customers = customers
+            };
+
+            return View(viewModel);
         }
 
-        [Route("books/released/{year}/{month}/{month:regex(\\d{4})}")]
+        /*[Route("books/released/{year}/{month}/{month:regex(\\d{4}):range(1, 12}")]
         public ActionResult ByReleaseDate(int year, int month)
         {
             return Content(year + "/" + month);
-        }
+        }*/
         
     }
 }
