46e9e58c83710879c0940cf1474dc2b4fbdf7c41
Cristian Plasencia <plc.cristian@gmail.com>
2021-01-28 22:16:22 +0000

    modelos para tipos y clientes

--- MiBiblioteca/Models/Customer.cs
+++ MiBiblioteca/Models/Customer.cs
@@ -9,5 +9,8 @@ namespace MiBiblioteca.Models
     {
         public int Id { get; set; }
         public String Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+        public MembershipType MembershipType { get; set; }
+        public byte MembershipTypeId { get; set; }
     }
 }
